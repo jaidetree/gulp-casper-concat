ff5ea9d0b998ed477a77f92e9e6c03418065d06d
Jay Zawrotny <jayzawrotny@gmail.com>
2016-01-19 20:03:36 -0500

    Added winston as a contributor. A lot of this follows his implementation.

--- src/TestBuilder.js
+++ src/TestBuilder.js
@@ -1,6 +1,11 @@
 import splicer from 'labeled-stream-splicer';
 import through from 'through2';
 
+import AssembleTransform './AssembleTransform';
+import ES6Transform from './ES6Transform';
+import ReadTransform from './ReadTransform';
+import StoreTransform from './StoreTransform';
+
 import { Duplex } from 'stream';
 
 /**
@@ -11,11 +16,14 @@ import { Duplex } from 'stream';
 class TestBuilder extends Duplex {
   isInitialized = false;
 
+  /**
+   * Our main workflow for our labeled stream splicer pipeline
+   */
   workflow = [
-    ['read', new ReadTransform, {}],
-    ['babel', new BabelTransform, {}],
-    ['store', new StoreTransform, {}],
-    ['assemble', new AssembleTransform, {}],
+    ['read', ReadTransform, {}],
+    ['es6', ES6Transform, {}],
+    ['store', StoreTransform, {}],
+    ['assemble', AssembleTransform, {}],
   ];
 
   /**
@@ -34,15 +42,30 @@ class TestBuilder extends Duplex {
     this.init();
   }
 
+  /**
+   * Initialize the input and output streams and add the listeners
+   */
   init () {
     if (this.isInitialized) return;
 
+    /**
+     * Setup this stream to pipe the input to the pipeline and out through
+     * our output passthrough stream. This will make this class a little
+     * easier to test.
+     */
     this.input
       .pipe(this.pipeline)
       .pipe(this.output);
 
     this.output
+      /**
+       * When output is readable: posh it's data down the stream
+       */
       .on('readable', this.readOutput.bind(this))
+      /**
+       * When the output ends tell the receiving stream that we are done
+       * pushing data.
+       */
       .on('end', () => {
         this.push(null);
       });
@@ -50,10 +73,33 @@ class TestBuilder extends Duplex {
     this.isInitialized = true;
   }
 
+  /**
+   * Return the assembled pipeline
+   *
+   * @returns {object} Labeled stream splicer instance built with our workflow
+   */
   createPipeline () {
-    return splicer();
+    let pipeline = [];
+
+    this.workflow.forEach(([name, Stream, opts]) => {
+      let stream = new Stream(opts);
+
+      pipeline.push(name);
+      pipeline.push(stream);
+
+      stream.on('error', (err) => {
+        console.error(err.stack || err.message || err)
+      });
+    });
+
+    return splicer(pipeline);
   }
 
+  /**
+   * Read Output
+   * Reads output from our output stream and pushes it down this current
+   * duplex stream.
+   */
   readOutput () {
     let chunk;
 
@@ -63,8 +109,15 @@ class TestBuilder extends Duplex {
     }
   }
 
+  /**
+   * Required duplex stream read implementation. This is read from our
+   * output stream.
+   */
   _read () {}
 
+  /**
+   * Required duplex stream write impelmentation. Writes to our input stream.
+   */
   _write (...args) {
     this.input.write(...args);
   }
